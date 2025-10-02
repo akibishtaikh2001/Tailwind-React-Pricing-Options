ea1f7b7aefb245a78a5867342327aadaf51bce35
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-10-02 23:22:50 +0600

    Add fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
+import { Suspense } from 'react'
 import './App.css'
 import DaisyNav from './Components/DaisyNav/DaisyNav'
 import NavBar from './Components/NavBar/NavBar'
+import PricingOption from './Components/PricingOption/PricingOption'
+
+const pricingPromise = fetch('pricingData.json').then(res => res.json());
 
 function App() {
 
 
   return (
-    <>  
+    <>
 
       <header>
         <NavBar></NavBar>
         {/* <DaisyNav></DaisyNav> */}
       </header>
       <main>
-
+        <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
+          <PricingOption pricingPromise={pricingPromise}></PricingOption>
+        </Suspense>
       </main>
 
 
